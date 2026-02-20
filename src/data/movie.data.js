c2b0f71e29ce3ee165287e9aa316256058679b17
Adrian Widłak <adrian.widlak@interia.pl>
2026-02-20 14:40:25 +0100

    Cors policy

--- src/data/movie.data.js
+++ src/data/movie.data.js
@@ -3,12 +3,14 @@ export const movies = [
     id: 1,
     title: "Avatar",
     director: "James Cameron",
-    likes: 1
+    likes: 1,
+    description: "Epicki film science fiction o konflikcie ludzi z rdzennymi mieszkańcami planety Pandora."
   },
   {
     id: 2,
     title: "Interstellar",
     director: "XYZ",
-    likes: 5
+    likes: 5,
+    description: "Film science fiction o grupie astronautów podróżujących przez tunel czasoprzestrzenny w poszukiwaniu nowego domu dla ludzkości."
   },
 ];
